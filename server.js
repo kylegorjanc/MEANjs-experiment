33924260b7e7ee5d7a46fcd1108416410febf889
Kyle Gorjanc <kylegorjanc@gmail.com>
2017-03-01 15:04:54 -0500

    updated server to 8080

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var express     = require('express'),
 var db = require('./config/db');
 
 //set port
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 // connect to mongoDB 
 mongoose.connect('mongodb://localhost/dev-des-proj');
